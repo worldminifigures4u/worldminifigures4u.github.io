6ba266a3d8a0f5e789d2e5e75ca7f35d9f382ea9
worldminifigures4u <worldminifigures4u@gmail.com>
2026-08-14 08:10:09 +0100

    Clarificar nomes dos filtros de fornecedores

--- fornecedores.js
+++ fornecedores.js
@@ -476,7 +476,7 @@ function renderizarLinhasFiltroMarcacaoFornecedor(filtros = null) {
 
         const grupoFornecedor = document.createElement("label");
         grupoFornecedor.className = "fornecedor-controle-filtro-compacto";
-        grupoFornecedor.textContent = "Fornecedor";
+        grupoFornecedor.textContent = "Ver marcacao em";
         grupoFornecedor.appendChild(criarSelectFiltroMarcacaoFornecedor(filtro.fornecedor));
 
         const grupoMarcacao = document.createElement("label");
