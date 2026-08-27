e93d4b61674a95d3bc25aab6f5ed00f04a1c6ecb
worldminifigures4u <worldminifigures4u@gmail.com>
2026-08-27 07:18:11 +0100

    Afinar campos de portes e total

--- admin-encomenda-loader.js
+++ admin-encomenda-loader.js
@@ -21,7 +21,7 @@
         if (!promessaVista) {
             promessaVista = carregarScript('morada-formato.js?v=20260713-morada-formatada')
                 .then(function () {
-                    return carregarScript('admin-encomenda-vista.js?v=20260827-portes-editaveis');
+                    return carregarScript('admin-encomenda-vista.js?v=20260827-valores-sem-limpar');
                 });
         }
         return promessaVista;
