69437d27319c0dbe70bcd739d00705f392fe8f1f
worldminifigures4u <worldminifigures4u@gmail.com>
2026-08-28 08:29:32 +0100

    Atualizar carregador da pagina de encomendas

--- encomendas-pagina.js
+++ encomendas-pagina.js
@@ -23,7 +23,7 @@
                 return carregarScript('paises-cliente.js?v=20260731-wallapop-es');
             })
             .then(function () {
-                return carregarScript('encomendas.js?v=20260827-concluir-lote-moloni');
+                return carregarScript('encomendas.js?v=20260828-selecao-todas-vistas');
             });
 
         return promessaEncomendasJs;
