dda0655ef0f1105a1232a172fad5263e4fc01fd0
worldminifigures4u <worldminifigures4u@gmail.com>
2026-09-02 19:44:00 +0100

    Remover ficha de cliente duplicada das encomendas

--- encomendas-pagina.js
+++ encomendas-pagina.js
@@ -23,7 +23,7 @@
                 return carregarScript('paises-cliente.js?v=20260731-wallapop-es');
             })
             .then(function () {
-                return carregarScript('encomendas.js?v=20260902-ficha-cliente-unica');
+                return carregarScript('encomendas.js?v=20260902-remover-ficha-cliente-duplicada');
             });
 
         return promessaEncomendasJs;
