c2bd46b70f7d8609649511e00b2dbb234da00a76
worldminifigures4u <worldminifigures4u@gmail.com>
2026-09-20 08:13:17 +0100

    Destacar editar da ficha de cliente

--- encomendas.js
+++ encomendas.js
@@ -264,7 +264,7 @@ function garantirFichaClienteEncomendas() {
         return Promise.resolve();
     }
     if (!promessaFichaClienteEncomendas) {
-        promessaFichaClienteEncomendas = carregarScriptEncomendasAdmin('admin-ficha-cliente.js?v=20260920-editar-topo')
+        promessaFichaClienteEncomendas = carregarScriptEncomendasAdmin('admin-ficha-cliente.js?v=20260920-editar-amarelo')
             .then(() => configurarFichaClienteEncomendas());
     }
     return promessaFichaClienteEncomendas;
--- plataforma.js
+++ plataforma.js
@@ -578,7 +578,7 @@ function garantirFichaClientePlataforma() {
                 return carregarScriptAdmin('paises-cliente.js?v=20260731-wallapop-es');
             })
             .then(function () {
-                return carregarScriptAdmin('admin-ficha-cliente.js?v=20260920-editar-topo');
+                return carregarScriptAdmin('admin-ficha-cliente.js?v=20260920-editar-amarelo');
             })
             .then(function () {
                 window.AdminFichaCliente?.configurar({
