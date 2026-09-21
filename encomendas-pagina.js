a83741c2a626bd5886dea54f831c47152377512c
worldminifigures4u <worldminifigures4u@gmail.com>
2026-09-21 08:28:17 +0100

    Padronizar estado no topo da encomenda

--- encomendas-pagina.js
+++ encomendas-pagina.js
@@ -23,7 +23,7 @@
                 return carregarScript('paises-cliente.js?v=20260731-wallapop-es');
             })
             .then(function () {
-                return carregarScript('encomendas.js?v=20260921-estado-topo');
+                return carregarScript('encomendas.js?v=20260921-estado-topo-css');
             });
 
         return promessaEncomendasJs;
