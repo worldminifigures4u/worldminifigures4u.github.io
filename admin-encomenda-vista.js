7919ad1b4fb43f81ef5da0cd9bc8a45d9c732842
worldminifigures4u <worldminifigures4u@gmail.com>
2026-09-21 09:03:01 +0100

    Dar base comum ao botao anexos

--- admin-encomenda-vista.js
+++ admin-encomenda-vista.js
@@ -1123,7 +1123,7 @@ window.AdminEncomendaVista = (function () {
             ));
         } else {
             const upload = criarElemento("div", "admin-encomenda-gestao-caixa admin-encomenda-anexos-upload");
-            const campoFicheiro = criarElemento("label", "admin-encomenda-anexos-escolher");
+            const campoFicheiro = criarElemento("label", "wallapop-botao admin-encomenda-anexos-escolher");
             const input = document.createElement("input");
             input.type = "file";
             input.className = "admin-encomenda-anexos-input";
